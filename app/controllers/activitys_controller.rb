4f4d268d53200742444f9a5eeeb8bafcc4a3ce2f
GreenHill <yoshimitsu41@gmail.com>
2016-09-02 18:39:08 +0900

    activitiesのview作成

--- app/controllers/activitys_controller.rb
+++ app/controllers/activitys_controller.rb
@@ -1,4 +1,27 @@
 class ActivitysController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    Activity.create(activity_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def activity_params
+  end
 end
