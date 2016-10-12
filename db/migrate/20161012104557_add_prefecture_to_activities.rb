eaf825bae9d11d69e2728c97eac891bb382f34f7
GreenHill <yoshimitsu41@gmail.com>
2016-10-12 20:11:14 +0900

    もう一度戻した

--- db/migrate/20161012104557_add_prefecture_to_activities.rb
+++
@@ -1,8 +0,0 @@
-class AddPrefectureToActivities < ActiveRecord::Migration
-  def change
-    add_column :activities, :zone, :string
-    add_column :activities, :prefecture, :string
-    add_column :activities, :city, :string
-    add_column :activities, :address, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012104557) do
+ActiveRecord::Schema.define(version: 20161012103941) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,10 +20,6 @@ ActiveRecord::Schema.define(version: 20161012104557) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "store_id",    limit: 4
-    t.string   "zone",        limit: 255
-    t.string   "prefecture",  limit: 255
-    t.string   "city",        limit: 255
-    t.string   "address",     limit: 255
   end
 
   create_table "activity_images", force: :cascade do |t|
